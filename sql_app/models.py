6f72ac3d262cf96f5eff840600b34ad61f4ffe57
wangy <2503308794@qq.com>
2021-05-08 20:07:30 +0800

    func: sql query api

---
+++ sql_app/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import BOOLEAN, INTEGER, String
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(INTEGER, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(BOOLEAN, default=True)
+
+    items = relationship("Item", back_populates="owner")
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(INTEGER, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    owner_id = Column(INTEGER, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="items")
